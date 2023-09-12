a583cae3e4845fff21d6b99576cba9107ab1e77f
jordie.juwono <jordie.juwono@indocyber.id>
2023-09-12 21:21:53 +0700

    update realm version to stable version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/realm/realm-swift.git", branch: "master")
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.42.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
